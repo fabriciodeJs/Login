a99acc16095781c6922b04ff1da3973ef03c742b
Fabricio de Jesus <114605554+fabriciodeJs@users.noreply.github.com>
2023-03-17 17:47:43 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function menuLateral() {
+    const menuMobile = document.querySelector('#icon');
+
+    if (imenuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    }else{
+        menuMobile.classList.add('open');
+    }
+}
+

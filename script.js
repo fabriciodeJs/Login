bf932993cfbfe6e8113e67b1db36c8ab139c2c7d
Fabricio de Jesus <114605554+fabriciodeJs@users.noreply.github.com>
2023-03-23 16:19:02 -0300

    Melhoria Responsividade

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-const btnMenu = document.querySelector('#btn-menu');
-const menuMobile = document.querySelector('.menu');
-
-
-btnMenu.addEventListener('click', (e) => {
-    if(menuMobile.style.display == 'none'){
-        menuMobile.style.display = 'flex'
-    }else{
-        menuMobile.style.display = 'none';
-    }
-});
-
 
 
